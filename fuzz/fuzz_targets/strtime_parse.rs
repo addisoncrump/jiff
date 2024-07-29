f683d9dc9c834c465cfe7613299805f90e706079
Addison Crump <addison.crump@cispa.de>
2024-07-29 15:49:53 +0200

    simplify arbitrary use

--- fuzz/fuzz_targets/strtime_parse.rs
+++ fuzz/fuzz_targets/strtime_parse.rs
@@ -1,10 +1,11 @@
 #![no_main]
 
 use libfuzzer_sys::arbitrary::{Arbitrary, Unstructured};
-use libfuzzer_sys::{arbitrary, fuzz_target, Corpus};
+use libfuzzer_sys::{arbitrary, fuzz_target};
 
 use jiff::fmt::strtime::parse;
 
+#[derive(Debug)]
 struct Input<'a> {
     format: &'a [u8],
     input: &'a [u8],
@@ -12,9 +13,10 @@ struct Input<'a> {
 
 impl<'a> Arbitrary<'a> for Input<'a> {
     fn arbitrary(u: &mut Unstructured<'a>) -> arbitrary::Result<Self> {
-        let len: u8 = u.arbitrary()?;
-        let format = u.bytes(len as usize)?;
-        let input = u.bytes(u.len())?;
+        let fmt_len: u8 = u.arbitrary()?;
+        let in_len: u16 = u.arbitrary()?;
+        let format = u.bytes(fmt_len as usize)?;
+        let input = u.bytes(in_len as usize)?;
         Ok(Input { format, input })
     }
 
@@ -22,19 +24,15 @@ impl<'a> Arbitrary<'a> for Input<'a> {
         mut u: Unstructured<'a>,
     ) -> arbitrary::Result<Self> {
         let len: u8 = u.arbitrary()?;
+        let _in_len: u16 = u.arbitrary()?; // ignored in take rest, but keep it consistent
         let format = u.bytes(len as usize)?;
         let input = u.take_rest();
         Ok(Input { format, input })
     }
 }
 
-fn do_fuzz(data: &[u8]) -> Corpus {
-    if let Ok(src) = Input::arbitrary_take_rest(Unstructured::new(data)) {
-        let _ = parse(src.format, src.input);
-        Corpus::Keep
-    } else {
-        Corpus::Reject
-    }
+fn do_fuzz(src: Input) {
+    let _ = parse(src.format, src.input);
 }
 
-fuzz_target!(|data: &[u8]| -> Corpus { do_fuzz(data) });
+fuzz_target!(|data: Input<'_>| do_fuzz(data));
